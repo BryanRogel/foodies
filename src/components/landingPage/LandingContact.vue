eea9159d33d1ee6ee744065899ffa40213efae35
Bryan Rogel <bryanrogeldev@gmail.com>
2020-08-12 18:12:13 -0600

    add(LandingHome):
    Landing Home layout.

---
+++ src/components/landingPage/LandingContact.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        name: 'LandingContact',
+        };
+    },
+}
+</script>
+
+<style lang="less" scoped>
+    
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <LandingHome/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,12 +8,14 @@
 
 <script>
 // @ is an alias to /src
+import LandingHome from '@/components/landingPage/LandingHome.vue'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    LandingHome
   }
 }
 </script>
